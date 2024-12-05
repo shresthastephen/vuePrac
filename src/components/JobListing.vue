d0e73a1c238c8a4df089a81fc543d2a57b61a864
Stephen Stha <stephencrestha07@gmail.com>
2024-12-05 19:57:04 +0545

    vue

---
+++ src/components/JobListing.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from "vue";
+</script>
+
+<template>
+  <div class="bg-white rounded-xl shadow-md relative">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-grey-600 my-2">Full Time</div>
+        <h3 class="text-xl font-bold">Senior Vue Developer</h3>
+      </div>
+
+      <div class="md-5">
+        We are seeking a talented Front-End developer to join our team in
+        Boston,MA. The ideal candidate will have strong skills in HTML/CSS & JS.
+      </div>
+    </div>
+  </div>
+</template>
