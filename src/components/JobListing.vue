42d171231725608be8a61697ddabd8652377d7ae
Stephen Stha <stephencrestha07@gmail.com>
2025-01-01 11:38:13 +0545

    commit

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,19 +1,12 @@
 <script setup>
-import { defineProps } from "vue";
+// import jobData from "@/jobs.json";
+// import { ref } from "vue";
+
+// const jobs = ref(jobData);
 </script>
 
 <template>
-  <div class="bg-white rounded-xl shadow-md relative">
-    <div class="p-4">
-      <div class="mb-6">
-        <div class="text-grey-600 my-2">Full Time</div>
-        <h3 class="text-xl font-bold">Senior Vue Developer</h3>
-      </div>
-
-      <div class="md-5">
-        We are seeking a talented Front-End developer to join our team in
-        Boston,MA. The ideal candidate will have strong skills in HTML/CSS & JS.
-      </div>
-    </div>
-  </div>
+  <!-- <div v-for="job in jobs" :key="job.id">
+    {{ job.title }}
+  </div> -->
 </template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,23 +1,14 @@
 <script setup>
-import jobData from "@/jobs.json";
+import jobData from "@/jobs.json"; // Import the JSON data
 import { ref } from "vue";
-import JobListing from "./JobListing.vue";
 
-const jobs = ref(jobData);
+const jobs = ref(jobData.jobs); // Use `ref` for reactive data
 </script>
 
 <template>
-  <section class="bg-blue-50 px-4 py-10">
-    <div class="container-xl lg:container m-auto">
-      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
-        Browse Jobs
-      </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs" :key="job.id" ;job="job" />
-      </div>
+  <div>
+    <div v-for="job in jobs" :key="job.id" class="job-card">
+      {{ job.title }}
     </div>
-  </section>
-  <div v-for="job in jobs" :key="job.id">
-    {{ job.title }}
   </div>
 </template>
