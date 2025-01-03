ced925f9d86a58954bc181c026ae30bc68214ca4
Stephen Stha <stephencrestha07@gmail.com>
2025-01-03 22:24:19 +0545

    showbuttonProps

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -10,6 +10,10 @@ defineProps({
     type: Number,
     required: true,
   },
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
 });
 // console.log(limit); // Use `ref` for reactive data
 </script>
@@ -28,5 +32,13 @@ defineProps({
         />
       </div>
     </div>
+  </section>
+
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
   </section>
 </template>
